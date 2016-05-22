496d30f5c0f6095168f5f8885b9a9ad5fabc806c
16348104 <16348104@QQ.com>
2016-05-22 19:11:30 +0800

    update canvas

---
+++ canvas_circle/canvas.js
@@ -0,0 +1,19 @@
+/**
+ * Created by xdx on 2016/5/22.
+ */
+function draw(id){
+    var canvas = document.getElementById(id);
+    var context = canvas.getContext("2d");
+    context.fillStyle = "#f1f2f3";
+    context.fillRect(0, 0, 400, 400);
+
+    for (var i= 0;i<10;i++) {
+        context.beginPath();
+        context.arc(i*20, i*20, i*10, 0, Math.PI * 2, true);
+        context.closePath();
+        context.fillStyle = "rgba(255,0,0,0.25)";
+        context.fill();
+        context.strokeStyle = "red";
+        context.stroke()
+    }
+}
