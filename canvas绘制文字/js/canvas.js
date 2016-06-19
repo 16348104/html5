96ed7a7efea098f8a182b43db35c1255d7794d1b
16348104 <16348104@qq.com>
2016-06-19 17:27:34 +0800

    update canvas

--- canvas绘制文字/js/canvas.js
+++ canvas绘制文字/js/canvas.js
@@ -1,3 +1,15 @@
-/**
- * Created by xdx on 2016/5/24.
- */
+
+function draw(id){
+    var canvas = document.getElementById(id);
+    var context = canvas.getContext('2d');
+    context.fillStyle='green';
+    context.fillRect(0, 0, 800, 300);
+    context.fillStyle='#fff';
+    // context.strokeStyle='#fff';
+    context.font = "bold 40px,'微软雅黑'";
+    context.textBaseline = 'hanging';
+    context.fillText('欢迎光临由麦子学院推出的',10,40,800);
+    context.fillText('《HTML5和CSS3》轻松入门与实践',50,110,800);
+    context.fillText('视频教程',580,180);
+    // context.strokeText('麦子学院',0,40);
+}
