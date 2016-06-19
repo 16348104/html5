d7ad06d13ddb4a64f87d7e8c7bd53c37ebd75860
16348104 <16348104@qq.com>
2016-06-19 18:08:30 +0800

    update canvas保存图片

---
+++ canvas图像保存/js/canvas.js
@@ -0,0 +1,8 @@
+function draw(id) {
+    var canvas = document.getElementById(id);
+    var context = canvas.getContext('2d');
+    context.fillStyle = 'blue';
+    context.fillRect(0, 0, 400, 300);
+    // window.location = canvas.toDataURL('image/png');
+    
+}
